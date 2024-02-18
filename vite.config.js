07a994ea5b6b90ebb6390c9a25875fbbad3bcdae
Aman <uk.amanbisht@gmail.com>
2024-02-18 16:27:19 +0530

    test

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindCss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
   plugins: [
     react(),
     // Use the Tailwind CSS plugin
     tailwindCss(),
-    // Use the Autoprefixer plugin
-    autoprefixer(),
   ],
 });
