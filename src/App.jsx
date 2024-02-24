ad46ca939cddc86dc8c144bf9911702a5e052b30
Aman <uk.amanbisht@gmail.com>
2024-02-24 17:40:52 +0530

    toast message

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import { Cursor } from "../src/components/cursor/cursor.component";
 const App = () => {
   return (
     <Fragment>
-
       <Home />
       <Cursor />
+      <div className="toast-box">
+        <h1 className="toast-heading">This website is currently under development</h1>
+      </div>
     </Fragment>
   );
 };
