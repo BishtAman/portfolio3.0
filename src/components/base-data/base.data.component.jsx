f9cba85c96dc9d790c6026a65c37584ec90be164
Aman <uk.amanbisht@gmail.com>
2024-02-24 13:30:22 +0530

    minor responsive fix

--- src/components/base-data/base.data.component.jsx
+++ src/components/base-data/base.data.component.jsx
@@ -29,7 +29,7 @@ export const BaseData = () => {
       <h1 className="text-[#00D9FF] text-[35px]  max-[1255px]:text-[28px] max-[1000px]:text-[24px] max-[500px]:text-[18px] max-[500px]:text-center">
         Hey, I&#39;m <span className="text-white white-phrase">Aman.</span>
       </h1>
-      <p className="text-white text-[60px] font-extrabold max-[1255px]:text-[50px] max-[1000px]:text-[35px] max-[500px]:text-[37px] max-[500px]:text-center">
+      <p className="text-white text-[60px] font-extrabold max-[1255px]:text-[50px] max-[1000px]:text-[35px] max-[500px]:text-[30px]  max-[500px]:text-center max-[400px]:mx-[20px] max-[400px]:text-[29px]">
         Uncovering New Realms of the Digital Cosmos, where&nbsp;
         <span className=" text-[#00D9FF] white-phrase">Code</span> and&nbsp;
         <span className=" text-[#00D9FF] white-phrase">Creativity</span>
