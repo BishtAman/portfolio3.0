05086f5bafbd184ec79683a63d88a7ea7f42224e
Aman <uk.amanbisht@gmail.com>
2024-02-24 13:45:09 +0530

    responsive fix

--- src/components/base-data/base.data.component.jsx
+++ src/components/base-data/base.data.component.jsx
@@ -3,6 +3,7 @@ import { ConnectButton } from "../connect-Button/connect-Button.component";
 import { motion } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
 import { useInView } from "react-intersection-observer";
+import './base-data.component.scss'
 
 export const BaseData = () => {
   const [ref, inView] = useInView();
@@ -24,12 +25,12 @@ export const BaseData = () => {
       initial={{ y: 50, scale: 0.2, opacity: 0 }}
       animate={inView ? { opacity: 1, scale: 1, y: 0 } : {}}
       transition={{ duration: 0.7, delay: animationDelay }}
-      className="phrase h-[100vh] w-[100%] flex justify-center pt-[100px] flex-col pl-[16rem] pr-[2rem] max-[1000px]:pl-[11rem] max-[1000px]:left-[75px] max-[700px]:pl-[8rem] max-[500px]:pl-[2rem] max-[500px]:pt-[10px] max-[500px]:items-center space-y-10"
+      className="phrase h-[100vh] w-[100%] flex justify-center pt-[100px] flex-col pl-[16rem] pr-[4rem] max-[1000px]:pl-[11rem] max-[1000px]:left-[75px] max-[700px]:pl-[8rem] max-[500px]:pl-[4rem] max-[500px]:pt-[10px] max-[500px]:items-center space-y-10"
     >
-      <h1 className="text-[#00D9FF] text-[35px]  max-[1255px]:text-[28px] max-[1000px]:text-[24px] max-[500px]:text-[18px] max-[500px]:text-center">
+      <h1 className="phrase-h text-[#00D9FF] text-[35px]  max-[1255px]:text-[28px] max-[1000px]:text-[24px] max-[500px]:text-[18px] max-[500px]:text-center">
         Hey, I&#39;m <span className="text-white white-phrase">Aman.</span>
       </h1>
-      <p className="text-white text-[60px] font-extrabold max-[1255px]:text-[50px] max-[1000px]:text-[35px] max-[500px]:text-[30px]  max-[500px]:text-center max-[400px]:mx-[20px] max-[400px]:text-[29px]">
+      <p className="phrase-p text-white text-[60px] font-extrabold max-[1255px]:text-[50px] max-[1000px]:text-[35px]  max-[500px]:text-center">
         Uncovering New Realms of the Digital Cosmos, where&nbsp;
         <span className=" text-[#00D9FF] white-phrase">Code</span> and&nbsp;
         <span className=" text-[#00D9FF] white-phrase">Creativity</span>
