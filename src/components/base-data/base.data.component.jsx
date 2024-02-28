f2f5bf909c32c276cf1b6b3ffc1ba8efc1ce25e5
Aman <uk.amanbisht@gmail.com>
2024-02-28 21:34:12 +0530

    test fixes

--- src/components/base-data/base.data.component.jsx
+++ src/components/base-data/base.data.component.jsx
@@ -3,7 +3,7 @@ import { ConnectButton } from "../connect-Button/connect-Button.component";
 import { motion } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
 import { useInView } from "react-intersection-observer";
-import "./base-data.component.scss";
+
 
 export const BaseData = () => {
   const [ref, inView] = useInView();
@@ -26,7 +26,7 @@ export const BaseData = () => {
         initial={{ y: 150, opacity: 0 }}
         animate={inView ? { opacity: 1, y: 0 } : {}}
         transition={{ duration: 0.7, delay: animationDelay }}
-        className="text-[#00D9FF] text-3xl sm:text-5xl lg:text-4xl leading-tight text-center"
+        className="text-[#00D9FF] text-2xl sm:text-5xl lg:text-4xl leading-tight text-center"
       >
         Hey, I&#39;m <span className="text-white">Aman.</span>
       </motion.h1>
@@ -35,7 +35,7 @@ export const BaseData = () => {
         initial={{ y: 150, opacity: 0 }}
         animate={inView ? { opacity: 1, y: 0 } : {}}
         transition={{ duration: 0.7, delay: animationDelay }}
-        className="text-white text-4xl sm:text-3xl lg:text-6xl text-center max-[450px]:text-left font-extrabold max-w-6xl"
+        className="text-white text-5xl sm:text-3xl lg:text-6xl text-center font-extrabold max-w-6xl"
       >
         Uncovering New Realms of the Digital Cosmos, where{" "}
         <span className="text-[#00D9FF]">Code</span> and{" "}
--- src/components/skills-head/skills-head.component.jsx
+++ src/components/skills-head/skills-head.component.jsx
@@ -2,12 +2,12 @@ import Rocket from "../../assets/rocket.png";
 import './skills-head.styles.scss' 
 export const SkillsHead = () => {
   return (
-    <h1 className="text-[60px] font-extrabold relative flex items-center [@media(max-width:520px)]:text-[50px] [@media(max-width:460px)]:text-[40px]">
+    <h1 className="text-[60px] font-extrabold relative flex items-center [@media(max-width:460px)]:text-[45px]">
       <span className="text-white heading-text tracking-wider">
         Skillset
       </span>
 
-      <div className="rocket-img mx-[20px] -rotate-12 -space-y-2">
+      <div className="rocket-img mx-[10px] -rotate-12 -space-y-2">
         <img src={Rocket} alt="rocket image" className="h-[50px]" />
         <div className="fireplace">
           <div className="blur">
