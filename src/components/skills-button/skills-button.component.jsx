0d44f02261051c5b43a57d2e2ce909a270f8f332
Aman <uk.amanbisht@gmail.com>
2024-02-28 17:23:31 +0530

    test

--- src/components/skills-button/skills-button.component.jsx
+++ src/components/skills-button/skills-button.component.jsx
@@ -16,7 +16,7 @@ export const SkillButton = ({ name, imageURL, color, className }) => {
       ref={ref}
       initial={{ y: 50, opacity: 0 }}
       animate={inView ? { opacity: 1, y: 0 } : {}}
-      transition={{ duration: 0.6 }}
+      transition={{ duration: 0.7 }}
       whileHover={{
         scale: 1.2,
         transition: { duration: 0.3 },
@@ -25,7 +25,7 @@ export const SkillButton = ({ name, imageURL, color, className }) => {
       className={`raise mx-[10px] my-[15px] skill-button flex items-center bg-[#${color}] ${className} space-x-[5px] rounded-lg w-fit px-[15px] py-[9px] max-[450px]:mx-[7px] max-[450px]:my-[12px] max-[400px]:px-[12px] max-[400px]:py-[7px]`}
     >
       <img className="h-[20px] max-[450px]:h-[17px]" src={imageURL} alt={name} />
-      <span className="text-white font-bold  max-[450px]:font-normal tracking-widest text-[12px]">{name}</span>
+      <span className="text-white skill-text font-normal  max-[450px]:font-normal tracking-widest max-[450px]:text-[12px]">{name}</span>
     </motion.div>
   );
 };
