0c72ca04aed94eaa707922157781241cfdb19dc2
Aman <uk.amanbisht@gmail.com>
2024-02-28 18:35:29 +0530

    minor changes

--- src/routes/base/base.component.jsx
+++ src/routes/base/base.component.jsx
@@ -1,12 +1,12 @@
 import { Fragment } from "react";
-import { BaseImages } from "../../components/base-images/base-images.component";
+// import { BaseImages } from "../../components/base-images/base-images.component";
 import { BaseData } from "../../components/base-data/base.data.component";
 // import { CapaImg } from "../../components/base-images/base-img2.component";
 
 export const Base = () => {
   return (
     <Fragment>
-      <BaseImages />
+      {/* <BaseImages /> */}
       <BaseData />
       {/* <CapaImg/> */}
     </Fragment>
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,9 +1,9 @@
 import { Loading } from "../../components/loading-text/loading-text.component";
 import { Curtains } from "../../components/opening-curtains/opening-curtains.component";
-import { About } from "../about/about.component";
 import { Base } from "../base/base.component";
 import { Navigation } from "../navigation/navigation-bar.component";
-import { Skills } from "../skills/skills.component";
+// import { About } from "../about/about.component";
+// import { Skills } from "../skills/skills.component";
 import "./home.styles.scss";
 
 export const Home = () => {
@@ -15,9 +15,9 @@ export const Home = () => {
       <Base />
       {/* <div className="h-[100vh] w-[100%] bg-slate-700"></div> */}
       {/* <div id="about" className="h-[70px] mt-[30px] max-[450]:h-[0px]"></div> */}
-      <About />
+      {/* <About /> */}
       {/* <div id="about" className="h-[70px] mt-[30px] max-[450]:h-[0px]"></div> */}
-      <Skills/>
+      {/* <Skills/> */}
     </div>
   );
 };
