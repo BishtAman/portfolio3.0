2363cd6c272ca0a867263abe0eacc29c7cf51cdc
Aman <uk.amanbisht@gmail.com>
2024-03-05 20:56:43 +0530

    minor change

--- src/components/about-img/about-img.component.jsx
+++ src/components/about-img/about-img.component.jsx
@@ -1,13 +1,20 @@
 import Frame from "../../assets/profileFrame.png";
 import Man from "../../assets/man.png";
 import { motion } from "framer-motion";
- 
+import { useEffect, useRef } from "react";
+import { useInView } from "react-intersection-observer";
 
-export const AboutImg = ({num}) => {
-
-  return (
-    (num===1)?
-      (<section className="relative flex justify-center [@media(max-width:900px)]:w-[0%] m-auto">
+export const AboutImg = ({ num }) => {
+  const [ref, inView] = useInView();
+  const hasAnimatedRef = useRef(false);
+  useEffect(() => {
+    if (inView && !hasAnimatedRef.current) {
+      // Set hasAnimatedRef to true to prevent animation from repeating
+      hasAnimatedRef.current = true;
+    }
+  }, [inView]);
+  return num === 1 ? (
+    <section className="relative flex justify-center [@media(max-width:900px)]:w-[0%] m-auto">
       <img
         src={Frame}
         alt="this is frame"
@@ -22,24 +29,25 @@ export const AboutImg = ({num}) => {
         alt="this is man"
         className="h-[400px] absolute top-[-40px] left-[20%] [@media(max-width:1170px)]:h-[350px] [@media(max-width:900px)]:hidden"
       />
-    </section>)
-    :
-    (<section className="relative flex justify-center [@media(min-width:900px)]:w-[0%] mx-auto ">
+    </section>
+  ) : (
+    <motion.section
+      ref={ref}
+      initial={{ x: -50, opacity: 0 }}
+      animate={inView ? { opacity: 1, x: 0 } : {}}
+      transition={{ duration: 0.7, delay: 0.2 }}
+      className="relative flex justify-center [@media(min-width:900px)]:w-[0%] mx-auto "
+    >
       <img
         src={Frame}
         alt="this is frame"
         className="h-[300px] my-10 opacity-[0.3]  [@media(max-width:1170px)]:h-[290px]  [@media(min-width:900px)]:hidden "
       />
-      <motion.img
-        whileHover={{
-          scale: 1.2,
-          transition: { duration: 0.3 },
-        }}
+      <img
         src={Man}
         alt="this is man"
         className="h-[300px] my-10 absolute top-[13px] left-[20%] [@media(max-width:1170px)]:h-[260px] [@media(min-width:900px)]:hidden"
       />
-    </section>)
-    
+    </motion.section>
   );
 };
