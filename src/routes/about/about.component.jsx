86996500ca2a5cc785c08acdb59c5e016b38cc04
Aman <uk.amanbisht@gmail.com>
2024-04-07 18:00:37 +0530

    resume

--- src/routes/about/about.component.jsx
+++ src/routes/about/about.component.jsx
@@ -1,5 +1,5 @@
 import "./about.styles.scss";
-import Resume from '../../assets/Resume.pdf'
+import amanbisht from '../../assets/amanbisht.pdf'
 import { AboutInfo } from "../../components/about-info/about-info.component";
 import { AboutImg } from "../../components/about-img/about-img.component";
 
@@ -17,11 +17,11 @@ export const About = () => {
 
       <section className="flex pl-[80px] [@media(max-width:600px)]:pl-[0px] [@media(max-width:520px)]:justify-center">
         <div>
+          <a href={amanbisht} download>
           <button className="slide_from_right">
-          <a href={Resume} target="_blank" download>
             <span id="skills" className="btn">DOWNLOAD RESUME</span>
-          </a>
           </button>
+          </a>
         </div>
       </section>
 
